daf8f5048dae30b72045362638d5bdc7ac79fc61
Khalil <khalilnajjar@gmail.com>
2020-02-15 22:24:18 -0500

    Updated GraphsExample.py
    Added functional descriptions to the  Vertex and Graph classes to this example.

--- GraphsExample.py
+++ GraphsExample.py
@@ -1,4 +1,17 @@
-from random import randrange
+# The Graph data structure implemented here is deisnged with the following in mind:
+
+# Vertex:
+# Uses a dictionary as an adjacency list to store connected vertices.
+# Connected vertex names are keys and the edge weights are values.
+# Has methods to add edges and return a list of connected vertices.
+
+# Graph:
+# Can be initialized as a directed graph, where edges are set in one direction.
+# Stores every vertex inside a dictionary
+#   Vertex data is the key and the vertex instance is the value.
+# Has methods to add vertices, edges between vertices, and determine if a path 
+# exists between two vertices.from random import randrange
+
 from GraphsGraph import Graph
 from GraphsVertex import Vertex
 
