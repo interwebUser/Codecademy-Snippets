1d87818bf23316aeb641db2292e1a79bd1da63b8
Khalil <khalilnajjar@gmail.com>
2020-02-20 15:29:22 -0500

    Created Queue.py
    This file contains the barebones code required to implement a queue data structure in python. Note that you must import the Node class in order to use this. See NodesAndLinkedLists.py

---
+++ Queue.py
@@ -0,0 +1,58 @@
+# This program creates a Queue data structure which transacts items (nodes) in a 
+# LIFO format given the maximum size set. Note that the node is imported from another file
+# to save real estate in this file. This program was written by Khalil Najjar as part
+# of the Codecademy "Career Path: Computer Science" Course, Section 8, Complex Data Structures 
+
+
+from NodesAndLinkedLists import Node
+
+class Queue:
+  def __init__(self, max_size=None):
+    self.head = None
+    self.tail = None
+    self.max_size = max_size
+    self.size = 0
+    
+  def enqueue(self, value):
+    if self.has_space():
+      item_to_add = Node(value)
+      if self.is_empty():
+        self.head = item_to_add
+        self.tail = item_to_add
+      else:
+        self.tail.set_next_node(item_to_add)
+        self.tail = item_to_add
+      self.size += 1
+    else:
+      print("Sorry, no more room!")
+         
+  def dequeue(self):
+    if self.get_size() > 0:
+      item_to_remove = self.head
+      if self.get_size() == 1:
+        self.head = None
+        self.tail = None
+      else:
+        self.head = self.head.get_next_node()
+      self.size -= 1
+      return item_to_remove.get_value()
+    else:
+      print("The queue is totally empty!")
+  
+  def peek(self):
+    if self.size > 0:
+      return self.head.get_value()
+    else:
+      print("Nothing in the queue!")
+  
+  def get_size(self):
+    return self.size
+  
+  def has_space(self):
+    if self.max_size == None:
+      return True
+    else:
+      return self.max_size > self.get_size()
+    
+  def is_empty(self):
+    return self.size == 0
