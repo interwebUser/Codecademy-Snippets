3688802246f1f7360d0e873ac8414e0cf8a8b2e3
Khalil <khalilnajjar@gmail.com>
2020-03-01 13:34:40 -0500

    Create search_BinaryIterative.py
    Iterative approach to binary search

---
+++ Algorithms/search_BinaryIterative.py
@@ -0,0 +1,30 @@
+def binary_search(sorted_list, target):
+  left_pointer = 0
+  right_pointer = len(sorted_list)
+  
+  # fill in the condition for the while loop
+  while left_pointer < right_pointer:
+    # calculate the middle index using the two pointers
+    mid_idx = (left_pointer + right_pointer) // 2
+    mid_val = sorted_list[mid_idx]
+    if mid_val == target:
+      return mid_idx
+      
+    # When the middle value is greater than the search value, we focus on the right half of the list.
+    if target < mid_val:
+      # set the right_pointer to the appropriate value
+      right_pointer = mid_idx
+    
+    # When the middle value is less than the search value, we focus on the left half of the list.
+    if target > mid_val:
+      # set the left_pointer to the appropriate value
+      left_pointer = mid_idx + 1
+  
+  return "Value not in list"
+
+# test cases
+print(binary_search([5,6,7,8,9], 9))
+print(binary_search([5,6,7,8,9], 10))
+print(binary_search([5,6,7,8,9], 8))
+print(binary_search([5,6,7,8,9], 4))
+print(binary_search([5,6,7,8,9], 6))
